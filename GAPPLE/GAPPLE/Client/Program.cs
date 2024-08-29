a46f9eb3e688d151a5c438ef9a4eaf5670dd8c8b
TiziBasso <91997480+TiziBasso@users.noreply.github.com>
2024-08-28 23:10:04 -0300

    Radzen

--- GAPPLE/GAPPLE/Client/Program.cs
+++ GAPPLE/GAPPLE/Client/Program.cs
@@ -1,11 +1,19 @@
 using GAPPLE.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddRadzenComponents();
+//builder.Services.AddBlazoredSessionStorage();
+//builder.Services.AddAuthorizationCore();
+//builder.Services.AddScoped<AuthenticationService>();
+//builder.Services.AddScoped<AuthenticationStateProvider>(x => x.GetService<AuthenticationService>());
+//builder.Services.AddScoped<UsuariosService>();
 
 await builder.Build().RunAsync();
