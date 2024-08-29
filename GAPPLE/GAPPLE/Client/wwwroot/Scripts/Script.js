bf286a77eb1cbd2428691e002b05d3fb633602ce
TiziBasso <91997480+TiziBasso@users.noreply.github.com>
2024-08-29 01:31:42 -0300

    PedidosV1

---
+++ GAPPLE/GAPPLE/Client/wwwroot/Scripts/Script.js
@@ -0,0 +1,76 @@
+﻿document.addEventListener('keydown', function (event) {
+    if (event.target.tagName.toLowerCase() === 'input') {
+        let currentInput = event.target;
+        let currentId = currentInput.id;
+        let parts = currentId.split('_');
+        let row = parseInt(parts[1]);
+        let col = parseInt(parts[2]);
+
+        if (event.key === 'ArrowRight') {
+            if (document.getElementById(`input_${row + 1}_${col}`) != null) {
+                row++;
+            } else {
+                let colAux = 0;
+                while (true) {
+                    if (document.getElementById(`input_${row + 1}_${colAux}`) != null) {
+                        colAux++;
+                    }
+                    else {
+                        col = colAux - 1;
+                        row++;
+                        break;
+                    }
+                }
+            }
+        } else if (event.key === 'ArrowLeft') {
+            if (document.getElementById(`input_${row - 1}_${col}`) != null) {
+                row--;
+            } else {
+                let colAux = 0;
+                while (true) {
+                    if (document.getElementById(`input_${row - 1}_${colAux}`) != null) {
+                        colAux++;
+                    }
+                    else {
+                        col = colAux - 1;
+                        row--;
+                        break;
+                    }
+                }
+            }
+        } else if (event.key === 'ArrowDown') {
+            if (document.getElementById(`input_${row}_${col + 1}`) != null) {
+                col++;
+            }
+            else {
+                row += 4;
+                col = 0;
+            }
+        } else if (event.key === 'ArrowUp') {
+            if (document.getElementById(`input_${row}_${col - 1}`) != null) {
+                col--;
+            } else {
+                row -= 4;
+                let colAux = 0;
+                while (true) {
+                    if (document.getElementById(`input_${row}_${colAux}`) != null) {
+                        colAux++;
+                    }
+                    else {
+                        col = colAux - 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Asegúrate de que la siguiente posición sea válida
+        let nextInputId = `input_${row}_${col}`;
+        let nextInput = document.getElementById(nextInputId);
+
+        if (nextInput) {
+            nextInput.focus();
+            //event.preventDefault(); // Previene la acción por defecto de la tecla
+        }
+    }
+});
