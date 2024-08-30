f53fb74d3a63070838820380f11f5644ba1c0b64
BrunoScimonelli <bscimonelli@lasmargaritas.com.ar>
2024-08-30 11:51:56 -0300

    Cositas

---
+++ GAPPLE/GAPPLE/Shared/Model/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAPPLE.Shared.Model
+{
+    public class Cliente
+    {
+       public string RazoSocial { get; set; }  
+       public string DNICuit { get; set; }  
+
+    }
+}
