6fbf56eb0f37e65a65b98477502f3c1d3d15a28c
TiziBasso <91997480+TiziBasso@users.noreply.github.com>
2024-09-01 02:47:46 -0300

    OrdenesSinBDFinish

---
+++ GAPPLE/GAPPLE/Client/Helpers/Opcion.cs
@@ -0,0 +1,21 @@
+﻿namespace GAPPLE.Client.Helpers
+{
+    public class Opcion
+    {
+        public object Id { get; set; }
+        public string Descripcion { get; set; }
+        public bool Check { get; set; }
+
+        public Opcion(object id, string descripcion) => (Id, Descripcion) = (id, descripcion);
+
+        public Opcion(char id, string descripcion) => (Id, Descripcion) = (id, descripcion);
+
+        public Opcion(string id, string descripcion) => (Id, Descripcion) = (id, descripcion);
+
+        public Opcion(int id, string descripcion, bool check = false) => (Id, Descripcion, Check) = (id, descripcion, check);
+
+        public Opcion(int id, string descripcion) => (Id, Descripcion) = (id, descripcion);
+
+        public Opcion() { }
+    }
+}
--- GAPPLE/GAPPLE/Client/wwwroot/Scripts/Script.js
+++ GAPPLE/GAPPLE/Client/wwwroot/Scripts/Script.js
@@ -21,14 +21,14 @@ window.onload = function iconoTema() {
         if (getCookie('tema') == null) {
             document.cookie = "tema=light;path=/;SameSite=lax;expires=Thu, 12 Aug 2049 20:47:11 UTC;";
         }
-       
+
         link1.rel = 'stylesheet';
         //link1.href = './css/component-light.css';
         link1.href = 'RadzenStyles/css/Material3.css?v=' + timestamp;
         head.insertBefore(link1, head.firstChild);
     } else {
         document.body.className = 'dark-theme';
-        
+
         link1.rel = 'stylesheet';
         //link1.href = './_content/Radzen.Blazor/css/dark.css';
         link1.href = 'RadzenStyles/css/Material3-Dark.css?v=' + timestamp;
@@ -46,6 +46,16 @@ function changetheme(modoOscuro) {
 }
 
 document.addEventListener('keydown', function (event) {
+    // Selecciona el elemento que contiene el grid
+    let gridElement = document.querySelector('.OrdenProductos');
+
+    // Obtiene el valor de la propiedad grid-template-columns
+    const gridStyles = window.getComputedStyle(gridElement);
+    const columns = gridStyles.getPropertyValue('grid-template-columns');
+
+    // Cuenta cuántas columnas hay (dividiendo por el espacio que hay entre las columnas)
+    const numberOfColumns = columns.split(' ').length;
+
     if (event.target.tagName.toLowerCase() === 'input') {
         let currentInput = event.target;
         let currentId = currentInput.id;
@@ -53,70 +63,72 @@ document.addEventListener('keydown', function (event) {
         let row = parseInt(parts[1]);
         let col = parseInt(parts[2]);
 
-        if (event.key === 'ArrowRight') {
-            if (document.getElementById(`input_${row + 1}_${col}`) != null) {
-                row++;
-            } else {
-                let colAux = 0;
-                while (true) {
-                    if (document.getElementById(`input_${row + 1}_${colAux}`) != null) {
-                        colAux++;
+        if (event.key === 'ArrowRight' || event.key === 'ArrowLeft' || event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+            if (event.key === 'ArrowRight') {
+                if (document.getElementById(`input_${row + 1}_${col}`) != null) {
+                    row++;
+                } else {
+                    let colAux = 0;
+                    while (true) {
+                        if (document.getElementById(`input_${row + 1}_${colAux}`) != null) {
+                            colAux++;
+                        }
+                        else {
+                            col = colAux - 1;
+                            row++;
+                            break;
+                        }
                     }
-                    else {
-                        col = colAux - 1;
-                        row++;
-                        break;
+                }
+            } else if (event.key === 'ArrowLeft') {
+                if (document.getElementById(`input_${row - 1}_${col}`) != null) {
+                    row--;
+                } else {
+                    let colAux = 0;
+                    while (true) {
+                        if (document.getElementById(`input_${row - 1}_${colAux}`) != null) {
+                            colAux++;
+                        }
+                        else {
+                            col = colAux - 1;
+                            row--;
+                            break;
+                        }
+                    }
+                }
+            } else if (event.key === 'ArrowDown') {
+                if (document.getElementById(`input_${row}_${col + 1}`) != null) {
+                    col++;
+                }
+                else {
+                    row += numberOfColumns;
+                    col = 0;
+                }
+            } else if (event.key === 'ArrowUp') {
+                if (document.getElementById(`input_${row}_${col - 1}`) != null) {
+                    col--;
+                } else {
+                    row -= numberOfColumns;
+                    let colAux = 0;
+                    while (true) {
+                        if (document.getElementById(`input_${row}_${colAux}`) != null) {
+                            colAux++;
+                        }
+                        else {
+                            col = colAux - 1;
+                            break;
+                        }
                     }
                 }
             }
-        } else if (event.key === 'ArrowLeft') {
-            if (document.getElementById(`input_${row - 1}_${col}`) != null) {
-                row--;
-            } else {
-                let colAux = 0;
-                while (true) {
-                    if (document.getElementById(`input_${row - 1}_${colAux}`) != null) {
-                        colAux++;
-                    }
-                    else {
-                        col = colAux - 1;
-                        row--;
-                        break;
-                    }
-                }
-            }
-        } else if (event.key === 'ArrowDown') {
-            if (document.getElementById(`input_${row}_${col + 1}`) != null) {
-                col++;
-            }
-            else {
-                row += 6;
-                col = 0;
-            }
-        } else if (event.key === 'ArrowUp') {
-            if (document.getElementById(`input_${row}_${col - 1}`) != null) {
-                col--;
-            } else {
-                row -= 6;
-                let colAux = 0;
-                while (true) {
-                    if (document.getElementById(`input_${row}_${colAux}`) != null) {
-                        colAux++;
-                    }
-                    else {
-                        col = colAux - 1;
-                        break;
-                    }
-                }
-            }
-        }
 
-        // Asegúrate de que la siguiente posición sea válida
-        let nextInputId = `input_${row}_${col}`;
-        let nextInput = document.getElementById(nextInputId);
+            // Asegúrate de que la siguiente posición sea válida
+            let nextInputId = `input_${row}_${col}`;
+            let nextInput = document.getElementById(nextInputId);
 
-        if (nextInput) {
-            nextInput.focus();
+            if (nextInput) {
+                nextInput.focus();
+            }
             event.preventDefault(); // Previene la acción por defecto de la tecla
         }
     }
