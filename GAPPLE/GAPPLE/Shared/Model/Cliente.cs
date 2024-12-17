62802237c57c926ac7aa643ee3fb5185ec2ca897
BrunoSc1 <bruno.scimonelli436@gmail.com>
2024-12-16 22:41:20 -0300

    Hecho

--- GAPPLE/GAPPLE/Shared/Model/Cliente.cs
+++ GAPPLE/GAPPLE/Shared/Model/Cliente.cs
@@ -17,6 +17,7 @@ namespace GAPPLE.Shared.Model
         //[JsonProperty("CLASIFICACION")]
         public string? Clasificacion { get; set; }
         public bool ClienteEspecial { get; set; }
+        public bool Activa { get; set; }
         public string? Observaciones { get; set; }
     }
 }
