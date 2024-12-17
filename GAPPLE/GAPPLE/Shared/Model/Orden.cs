4c6ae602c32236c586591c54325d4b8d40d93ca6
ChrisAlf27 <christianalf8@gmail.com>
2024-12-16 22:56:11 -0300

    compilando

---
+++ GAPPLE/GAPPLE/Shared/Model/Orden.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAPPLE.Shared.Model
+{
+    public class Orden
+    {
+        public int Id { get; set; }
+
+        public string? Linea { get; set; }
+
+        public bool Factura { get; set; }
+
+        public bool Presupuesto { get; set; }
+
+        public bool Probadores { get; set; }
+
+        public bool Obsequios { get; set; }
+
+        public bool Exhibidor { get; set; }
+
+        public List<OrdenDetalle>? Detalle { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar un cliente")]
+        public string? Cliente { get; set; }
+
+        public string? Transporte { get; set; }
+
+        public string? CodTransporte { get; set; }
+
+        public int IdListaPrecio { get; set; }
+
+        public int Zona { get; set; }
+
+        public string? CondicionVenta { get; set; }
+
+        public int? IdOferta { get; set; }
+
+        public string? Entrega { get; set; }
+
+        public string? Notas { get; set; }
+
+        public DateTime? FechaEntrega { get; set; }
+
+        public DateTime? Creacion { get; set; }
+
+        public int? IdEstado { get; set; }
+
+        public string? IdTango { get; set; }
+
+        public int Unidades { get; set; }
+
+        public bool Aprobado { get; set; }
+    }
+
+    public class OrdenDetalle
+    {
+        public int Id { get; set; }
+
+        public string? IdProducto { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public decimal Descuento1 { get; set; }
+
+        public decimal Descuento2 { get; set; }
+
+        public decimal TotalDescuento
+        {
+            get
+            {
+                decimal factor1 = 1 - (Descuento1 / 100);
+                decimal factor2 = 1 - (Descuento2 / 100);
+                decimal descuentoFinal = 1 - (factor1 * factor2);
+                return descuentoFinal * 100;
+            }
+        }
+
+    }
+}
